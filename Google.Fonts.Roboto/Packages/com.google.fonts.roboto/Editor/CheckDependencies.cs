bd1c3a3b0592ead97b9cb994d1089d92834f1f3f
Stephen Hodgson <hodgson.designs@gmail.com>
2021-06-28 10:04:18 -0400

    initial project setup

---
+++ Google.Fonts.Roboto/Packages/com.google.fonts.roboto/Editor/CheckDependencies.cs
@@ -0,0 +1,59 @@
+﻿// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.IO;
+using TMPro.EditorUtilities;
+using UnityEditor;
+using UnityEngine;
+
+namespace Google.Fonts
+{
+    internal static class CheckDependencies
+    {
+        [InitializeOnLoadMethod]
+        public static void Check()
+        {
+            if (!Directory.Exists($"{Application.dataPath}/TextMesh Pro"))
+            {
+                if (EditorUtility.DisplayDialog("Missing Package!",
+                    "The project requires the Text Mesh Pro essential resources, would you like to import them now?",
+                    "OK", "Later"))
+                {
+                    ImportTMProEssentialResources();
+                }
+            }
+        }
+
+        // Copied straight from the TMP_PackageUtilities.cs but we needed to import the assets if in batch mode.
+        private static void ImportTMProEssentialResources()
+        {
+            // Check if the TMP Settings asset is already present in the project.
+            var settings = AssetDatabase.FindAssets("t:TMP_Settings");
+
+            string settingsFilePath;
+            byte[] settingsBackup;
+
+            if (settings.Length > 0)
+            {
+                // Save assets just in case the TMP Setting were modified before import.
+                AssetDatabase.SaveAssets();
+
+                // Copy existing TMP Settings asset to a byte[]
+                settingsFilePath = AssetDatabase.GUIDToAssetPath(settings[0]);
+                settingsBackup = File.ReadAllBytes(settingsFilePath);
+
+                AssetDatabase.importPackageCompleted += ImportCallback;
+            }
+
+            AssetDatabase.ImportPackage($"{TMP_EditorUtility.packageFullPath}/Package Resources/TMP Essential Resources.unitypackage", Application.isBatchMode);
+
+            void ImportCallback(string packageName)
+            {
+                // Restore backup of TMP Settings from byte[]
+                File.WriteAllBytes(settingsFilePath, settingsBackup);
+                AssetDatabase.Refresh();
+                AssetDatabase.importPackageCompleted -= ImportCallback;
+            }
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,11 +1,34 @@
-# upm-template
+# com.google.fonts.roboto
 
-A Unity package manager repository template for quickly creating and setting up new UPM package projects in Unity.
+[![openupm](https://img.shields.io/npm/v/com.google.fonts.roboto?label=openupm&registry_uri=https://package.openupm.com)](https://openupm.com/packages/com.google.fonts.roboto/)
+
+A Google.Fonts.Roboto package for the [Unity](https://unity.com/) Game Engine.
+
+## Installing
+
+### Via Unity Package Manager and OpenUPM
+
+- Open your Unity project settings
+- Select the `Package Manager`
+![scoped-registries](Documentation~/images/package-manager-scopes.png)
+- Add the OpenUPM package registry:
+  - `Name: OpenUPM`
+  - `URL: https://package.openupm.com`
+  - `Scope(s):`
+    - `com.google.fonts.roboto`
+- Open the Unity Package Manager window
+- Change the Registry from Unity to `My Registries`
+- Add the `Google.Fonts.Roboto` package
+
+### Via Unity Package Manager and Git url
+
+- Open your Unity Package Manager
+- Add package from git url: `https://github.com/Stephen Hodgson/com.google.fonts.roboto.git#upm`
 
 ## Getting Started
 
-1. Create a new repository in GitHub using this template
-2. Clone to your local machine
-3. Run the `InitializeTemplate.ps1` script from powershell, a command line, or terminal
-4. ???
-5. Profit
+### Project Setup
+
+```csharp
+// TODO
+```
