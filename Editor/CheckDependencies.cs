445d6d5530a6cd003d10888931bb2a40f23d9f4b
Stephen Hodgson <hodgson.designs@gmail.com>
2021-06-28 20:27:42 -0400

    Renamed package to com.fonts.roboto

---
+++ Editor/CheckDependencies.cs
@@ -0,0 +1,95 @@
+﻿// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.IO;
+using TMPro.EditorUtilities;
+using UnityEditor;
+using UnityEngine;
+
+namespace Fonts
+{
+    internal static class CheckDependencies
+    {
+        private static readonly string FontPath = "submodules\\google.fonts\\apache\\roboto\\static";
+        private static readonly string PackagePath = "Packages\\com.fonts.roboto";
+
+        [InitializeOnLoadMethod]
+        public static void Check()
+        {
+            if (!Directory.Exists($"{Application.dataPath}/TextMesh Pro"))
+            {
+                if (EditorUtility.DisplayDialog("Missing Package!",
+                    "The project requires the Text Mesh Pro essential resources, would you like to import them now?",
+                    "OK", "Later"))
+                {
+                    ImportTMProEssentialResources();
+                }
+            }
+
+            var projectRootPath = Directory.GetParent(Directory.GetParent(Application.dataPath).FullName);
+            var submoduleFontPath = $"{projectRootPath}\\{FontPath}";
+
+            if (!Directory.Exists(submoduleFontPath))
+            {
+                // Must be in a published package.
+                return;
+            }
+
+            var packagePath = Directory.GetParent(Application.dataPath);
+            var packageFontPath = $"{packagePath}\\{PackagePath}\\Runtime\\Fonts";
+
+            if (!Directory.Exists(packageFontPath))
+            {
+                Directory.CreateDirectory(packageFontPath);
+
+                var fonts = Directory.GetFiles(submoduleFontPath);
+
+                foreach (var font in fonts)
+                {
+                    var newPath = font.Replace(submoduleFontPath, packageFontPath);
+
+                    if (File.Exists(newPath))
+                    {
+                        File.Delete(newPath);
+                    }
+
+                    File.Copy(font, newPath);
+                }
+
+                AssetDatabase.Refresh(ImportAssetOptions.Default);
+            }
+        }
+
+        // Copied straight from the TMP_PackageUtilities.cs but we needed to import the assets if in batch mode.
+        private static void ImportTMProEssentialResources()
+        {
+            // Check if the TMP Settings asset is already present in the project.
+            var settings = AssetDatabase.FindAssets("t:TMP_Settings");
+
+            string settingsFilePath;
+            byte[] settingsBackup;
+
+            if (settings.Length > 0)
+            {
+                // Save assets just in case the TMP Setting were modified before import.
+                AssetDatabase.SaveAssets();
+
+                // Copy existing TMP Settings asset to a byte[]
+                settingsFilePath = AssetDatabase.GUIDToAssetPath(settings[0]);
+                settingsBackup = File.ReadAllBytes(settingsFilePath);
+
+                AssetDatabase.importPackageCompleted += ImportCallback;
+            }
+
+            AssetDatabase.ImportPackage($"{TMP_EditorUtility.packageFullPath}/Package Resources/TMP Essential Resources.unitypackage", Application.isBatchMode);
+
+            void ImportCallback(string packageName)
+            {
+                // Restore backup of TMP Settings from byte[]
+                File.WriteAllBytes(settingsFilePath, settingsBackup);
+                AssetDatabase.Refresh();
+                AssetDatabase.importPackageCompleted -= ImportCallback;
+            }
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+# com.fonts.roboto
+
+[![openupm](https://img.shields.io/npm/v/com.fonts.roboto?label=openupm&registry_uri=https://package.openupm.com)](https://openupm.com/packages/com.fonts.roboto/)
+
+A [Roboto font](https://fonts.google.com/specimen/Roboto) upm package for the [Unity](https://unity.com/) Game Engine.
+
+***All copyrights, trademarks, logos, and assets are the property of their respective owners.***
+
+## Installing
+
+### Via Unity Package Manager and OpenUPM
+
+- Open your Unity project settings
+- Select the `Package Manager`
+![scoped-registries](Documentation~/images/package-manager-scopes.png)
+- Add the OpenUPM package registry:
+  - `Name: OpenUPM`
+  - `URL: https://package.openupm.com`
+  - `Scope(s):`
+    - `com.fonts`
+- Open the Unity Package Manager window
+- Change the Registry from Unity to `My Registries`
+- Add the `Roboto Font` package
+
+### Via Unity Package Manager and Git url
+
+- Open your Unity Package Manager
+- Add package from git url: `https://github.com/RageAgainstThePixel/com.fonts.roboto.git#upm`
